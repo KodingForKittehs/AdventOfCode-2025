990b13becd144625adcf3f504e04afea65908ffb
KodingForKittehs <>
2025-12-01 21:52:27 -0500

    get rid of echo

--- bash/day_01.sh
+++ bash/day_01.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cz () { awk -v c=50 '{c=(c+$0)%100;p+=(c==0?1:0)} END {print p}'; }
+cz () { awk -v c=50 -v pt=$1 '{c=(c+$0)%100;p+=(c==0?1:0)} END {print "Part " pt ": " p}'; }
 poo () { awk '{d=$1>0?1:-1;for(i=0;i<d*$1;i++) print d}'; }
 
 cat "input.txt" | tr 'L' '-' | tr 'R' '+' | tee \
->(echo Part 1: $(cz)) \
->(poo | echo Part 2: $(cz)) \
+>(cz 1) \
+>(poo | cz 2) \
 > /dev/null
