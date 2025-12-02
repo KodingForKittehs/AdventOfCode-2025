bf413db7bfa8a521b201de91bd8b34df5006c68a
KodingForKittehs <>
2025-12-01 21:43:51 -0500

    make ugly

--- bash/day_01.sh
+++ bash/day_01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cz () { awk -v c=50 '{c=(c+$0)%100;p+=(c==0?1:0)} END {print p}'; }
-poo () { awk '{d = $1 > 0 ? 1 : -1; for (i = 0; i < d * $1; i++) print d}'; }
+poo () { awk '{d=$1>0?1:-1;for(i=0;i<d*$1;i++) print d}'; }
 
 cat "input.txt" | tr 'L' '-' | tr 'R' '+' | tee \
 >(echo Part 1: $(cz)) \
