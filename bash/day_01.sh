4a9bd7ff41489150b45992227861a6263731eb6d
KodingForKittehs <>
2025-12-01 22:15:03 -0500

    readbility, very improtant

--- bash/day_01.sh
+++ bash/day_01.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cz () { awk -v c=50 -v pt=$1 '{c=(c+$0)%100;p+=(c==0?1:0)} END {print "Part " pt ": " p}'; }
-poo () { awk '{d=$1>0?1:-1;for(i=0;i<d*$1;i++) print d}'; }
+count_cycles () { awk -v c=50 -v pt=$1 '{c=(c+$0)%100;p+=(c==0?1:0)} END {print "Part " pt ": " p}'; }
+expand_moves () { awk '{d=$1>0?1:-1;for(i=0;i<d*$1;i++) print d}'; }
 
 cat "input.txt" | tr 'L' '-' | tr 'R' '+' | tee \
->(cz 1) \
->(poo | cz 2) \
+>(count_cycles 1) \
+>(expand_moves | count_cycles 2) \
 > /dev/null
