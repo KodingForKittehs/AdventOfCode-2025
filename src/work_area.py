c97f5d4ab55184cf333efb46d7ba5aba134ebf9b
KodingForKittehs <>
2025-12-03 23:17:38 -0500

    cleanup work area

--- src/work_area.py
+++ src/work_area.py
@@ -1,36 +1,10 @@
-# pylint: disable=trailing-whitespace, missing-module-docstring, missing-function-docstring, missing-class-docstring, missing-final-newline, invalid-name, trailing-newlines, wrong-import-position, unused-import, redefined-outer-name
-import collections
-import functools
-from collections import defaultdict
-from itertools import combinations, permutations, product
-import heapq
-import itertools
-import importlib
+# pylint: disable=W0611, C0116, C0413, C0114
+# ruff: noqa: F401
 import sys
-import time
-import re
-
-import igraph
-import matplotlib.pyplot as plt
-import numpy as np
-
-# import pygame
-import shapely
-import shapely.plotting
-import z3
-
 sys.path.append("./")
 import src.kittehs_funkollection as kf
 
-importlib.reload(kf)
-# np.set_printoptions(threshold=sys.maxsize)
-np.set_printoptions(linewidth=sys.maxsize)
-np.set_printoptions(edgeitems=10)
+INPUT_FILE = "input.txt"
 
-inp = "input.txt"
-
-lines = kf.eat(inp)
-print(lines)
-grid = kf.to_grid(lines, ctype=float)
-
-print(grid)
+lines = kf.eat(INPUT_FILE)
+kf.print_grid(lines)
